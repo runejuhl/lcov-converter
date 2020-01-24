65fbfe7a69e5a289bed344a6de722b4ac8bfa7da
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2020-01-24 01:18:22 +0100

    Seemingly working

--- main.go
+++ main.go
@@ -1,14 +1,47 @@
 package main
 
 import (
+	"bytes"
+	"io"
+	"io/ioutil"
 	"log"
 
-	"github.com/codeclimate/test-reporter/env"
+	"github.com/codeclimate/test-reporter/formatters"
 	"github.com/codeclimate/test-reporter/formatters/lcov"
 )
 
-// import "bytes"
-// import "encoding/json"
+// CoverageFormatter is stolen
+type CoverageFormatter struct {
+	CoveragePath string
+	In           formatters.Formatter
+	InputType    string
+	Output       string
+	Prefix       string
+	AddPrefix    string
+	writer       io.Writer
+}
+
+// Save is stolen
+func (f CoverageFormatter) Save() error {
+	rep, err := f.In.Format()
+	if err != nil {
+		log.Fatalf("err: %s", err)
+	}
+
+	if len(rep.SourceFiles) == 0 {
+		log.Fatalf("could not find coverage info for source files: %s", err)
+	}
+
+	if f.writer == nil {
+		log.Fatalf("missing writer")
+	}
+
+	err = rep.Save(f.writer)
+	if err != nil {
+		log.Fatalf("could not save file: %s", err)
+	}
+	return nil
+}
 
 func main() {
 	formatter := lcov.Formatter{
@@ -28,8 +61,55 @@ func main() {
 
 	log.Printf("reports: %+v\n", report)
 
-	var gitHead, _ = env.GetHead()
+	// e, err := env.New()
+	// if err != nil {
+	//	log.Fatalf("err: %s", err)
+	// }
 
-	log.Printf("git: %+v\n", gitHead)
+	// var gitHead, _ = env.GetHead()
+	// log.Printf("git: %+v\n", gitHead)
 
+	// m := map[string]interface{}{}
+
+	// g := structs.Map(e.Git)
+	// for k, v := range g {
+	//	m[k] = v
+	// }
+
+	// g = structs.Map(e.CI)
+	// for k, v := range g {
+	//	m[k] = v
+	// }
+
+	// j, err := json.Marshal(m)
+	// if err != nil {
+	//	log.Fatalf("err: %s", err)
+	// }
+
+	// j, err := e.MarshalJSON()
+	// if err != nil {
+	//	log.Fatalf("err: %s", err)
+	// }
+	// log.Printf("json: %s", j)
+	// ioutil.WriteFile("climate.json", j, 0644)
+
+	bb := &bytes.Buffer{}
+
+	cf := CoverageFormatter{
+		Prefix:    ".",
+		InputType: "lcov",
+		writer:    bb,
+		In: &lcov.Formatter{
+			Path: "target/coverage/lcov.info",
+		},
+		Output: "-",
+	}
+
+	log.Printf("would write now: %+v", cf)
+	err = cf.Save()
+	if err != nil {
+		log.Fatalf("runFormatter failed: %s", err)
+	}
+
+	ioutil.WriteFile("climate.json", bb.Bytes(), 0644)
 }
